dc29c9f766213bfc6dea3d4dc17424485aa4de3d
zeraasu <matthewzerrath@hotmail.com>
2021-11-29 17:32:19 -0800

    Add project files.

---
+++ Adventurer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventurerApp
+{
+    class Adventurer
+    {
+        // Data members
+        string name = "Adventurer";
+        int dungeonsRaided = 0;
+        bool hasFlashlight = false;
+        float runningSpeed = 0f;
+        decimal totalMoney = 0m;
+
+        // Default constructor
+        public Adventurer()
+        {
+        }
+
+        // Constructor
+        public Adventurer(string aName, int aDungeonsRaided, bool aHasFlashlight, float aRunningSpeed, decimal aTotalMoney)
+        {
+            name = aName;
+            dungeonsRaided = aDungeonsRaided;
+            hasFlashlight = aHasFlashlight;
+            runningSpeed = aRunningSpeed;
+            totalMoney = aTotalMoney;
+        }
+
+        // Properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int DungeonsRaided
+        {
+            get
+            {
+                return dungeonsRaided;
+            }
+            set
+            {
+                dungeonsRaided = value;
+            }
+        }
+
+        public bool HasFlashlight
+        {
+            get
+            {
+                return hasFlashlight;
+            }
+            set
+            {
+                hasFlashlight = value;
+            }
+        }
+
+        public float RunningSpeed
+        {
+            get
+            {
+                return runningSpeed;
+            }
+            set
+            {
+                runningSpeed = value;
+            }
+        }
+
+        public decimal TotalMoney
+        {
+            get
+            {
+                return totalMoney;
+            }
+            set
+            {
+                totalMoney = value;
+            }
+        }
+
+        // Instance method
+        public override string ToString()
+        {
+            return "Name: " + name +
+                   ", Dungeons Raided: " + dungeonsRaided +
+                   ", Has a Flashlight: " + hasFlashlight +
+                   ", Running Speed: " + runningSpeed +
+                   "m/s, Total Money on hand: $" + totalMoney;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace AdventurerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adventurerName = "";
+            int numberOfDungeons = 0;
+            bool flashlight = false;
+            float speed = 0f;
+            decimal money = 0m;
+
+            WriteLine("Welcome to the Adventurer Recruitment Centre!");
+            WriteLine("If you wish to join our ranks, you will need to sign up with us.\n");
+
+            // Gathers data and overwrites blank data members with pass by reference
+            GetData(ref adventurerName, ref numberOfDungeons,
+                    ref flashlight, ref speed, ref money);
+
+            Adventurer applicant = new Adventurer(adventurerName, numberOfDungeons,
+                                                  flashlight, speed, money);
+
+            WriteLine("\nThank you for your application. Here is what you wrote:\n");
+            WriteLine(applicant);
+            
+        }
+
+        // Prompts the user for information about adventurer
+        static void GetData(ref string adventurerName, ref int numberOfDungeons,
+                            ref bool flashlight, ref float speed, ref decimal money)
+        {
+            Write("What is your name? ");
+            adventurerName = ReadLine();
+            Write("How many dungeons did you already explore? ");
+            numberOfDungeons = int.Parse(ReadLine());
+            Write("Is it true that you have a flashlight? ");
+            flashlight = bool.Parse(ReadLine());
+            Write("How fast can you run? ");
+            speed = float.Parse(ReadLine());
+            Write("How much money do you have on hand? ");
+            money = decimal.Parse(ReadLine());
+        }
+    }
+}
